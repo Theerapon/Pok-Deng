75a5d45db9f84d469945b55d5c5a36d8d9f68f6b
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-11-02 17:10:43 +0700

    v.2 cannot handler input string for bet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const {deck, card} = require('./entities/deck')
 
 
 const readline = require("readline");
+const readline2 = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,31 +16,42 @@ var currentBet = 0
 
 const putOnBet = () => {
     console.log('Please put your bet');
-    rl.question("", (answer) => {
-        currentBet = answer
-        rl.close()
-        dealOutCards()
-      });
+    return new Promise((resolve, reject) => {
+        rl.question("", (answer) => {
+            currentBet = parseInt(answer)
+            resolve()   
+          });
+    })
 }
 
 const displayPlayerCard = (player) => {
     console.log("You got " +  
-    comareCard(player.cards[0].face, player.cards[0].symbol) + " " +
-    comareCard(player.cards[1].face, player.cards[1].symbol) + " ")
+    compareCardToString(player.cards[0].face, player.cards[0].symbol) + " " +
+    compareCardToString(player.cards[1].face, player.cards[1].symbol))
+
+    player.totalScore = calFinalScore((player.cards[0].value + player.cards[1].value))
+    // console.log("player total score: " + player.totalScore)
 }
 
 const displayDealerCard = (dealer) => {
-    console.log("You got " +  
-    comareCard(dealer.cards[0].face, dealer.cards[0].symbol) + " " +
-    comareCard(dealer.cards[1].face, dealer.cards[1].symbol))
+    console.log("The dealer got " +  
+    compareCardToString(dealer.cards[0].face, dealer.cards[0].symbol) + " " +
+    compareCardToString(dealer.cards[1].face, dealer.cards[1].symbol))
+
+    dealer.totalScore = calFinalScore((dealer.cards[0].value + dealer.cards[1].value))
+    // console.log("dealer total score: " + dealer.totalScore)
 } 
 
-const comareCard = (face, symbol) => {
+const compareCardToString = (face, symbol) => {
     return deck.symbol[symbol] + "-" + deck.face[face];
 }
 
 const randomNumber = (start, end) => {
     return Math.floor(Math.random() * end) + start
+}
+
+const calFinalScore = (score) => {
+    return score % 10
 }
 
 const checkCardsExist = (outCardsFace, outCardsSymbol) => {
@@ -73,6 +85,23 @@ const checkCardsExist = (outCardsFace, outCardsSymbol) => {
 
 }
 
+const wannaPlay = () => {
+    console.log('Wanna play');
+    return new Promise((resolve, reject) => {
+        rl.question("", (answer) => {
+            
+            if (answer.trim().toLowerCase() === 'yes') {
+                resolve(true)
+                return 
+            } else if (answer.trim().toLowerCase() === 'no') {
+                rl.close()
+                resolve(false)
+                return 
+            }
+            wannaPlay()
+          });
+    })
+}
 
 const dealOutCards = () => {
     var outCardsFace = []
@@ -88,45 +117,83 @@ const dealOutCards = () => {
     player.cards.push( {
         face: outCardsFace[0],
         symbol: outCardsSymbol[0],
-        value: checkScore(outCardsFace[0])
+        value: calScore(outCardsFace[0])
     })
 
     player.cards.push( {
         face: outCardsFace[2],
         symbol: outCardsSymbol[2],
-        value: checkScore(outCardsFace[2])
+        value: calScore(outCardsFace[2])
     })
 
     dealer.cards.push( {
         face: outCardsFace[1],
         symbol: outCardsSymbol[1],
-        value: checkScore(outCardsFace[1])
+        value: calScore(outCardsFace[1])
     })
 
     dealer.cards.push( {
         face: outCardsFace[3],
         symbol: outCardsSymbol[3],
-        value: checkScore(outCardsFace[3])
+        value: calScore(outCardsFace[3])
     })
 
     displayPlayerCard(player)
     displayDealerCard(dealer)
 
-    checkScore()
-    
 }
 
-const checkScore = (face) => {
-    var score = 0
-    if (face === 'A') {
+const calScore = (index) => {
+    var score = 0;
+    if (index === 0) {
         score = 1;
-    } else if (face === 'Clubs' || face === 'Diamonds' || face === 'Hearts' || face === 'Spades') {
-        score = 10;
+    } else if (index === 9 || index === 10 || index === 11 || index === 12) {
+        score = 0;
     } else {
-        score = parseInt(face);
+        score = index + 1;
     }
     return score;
 }
 
-putOnBet()
+
+const compareScore = (player, dealer, currentBet) => {
+    let playerScore = player.totalScore;
+    let dealerScore = dealer.totalScore;
+    let lostBet = true;
+    if (playerScore > dealerScore) {
+        console.log("You won!!!, received " + currentBet)
+        lostBet = false;
+    } else if (playerScore === dealerScore) {
+        console.log("You draw!!!, missed " + currentBet)
+    } else {
+        console.log("You lost!!!, missed " + currentBet)
+    }
+
+    if (!lostBet) {
+        player.totalChips += currentBet;
+    } else {
+        player.totalChips -= currentBet;
+    }
+}
+
+const playGame = () => {
+
+    putOnBet().then(() => {
+        dealOutCards()
+        compareScore(player, dealer, currentBet)
+        wannaPlay().then((result) => {
+            console.log("You got total " + player.totalChips + " chips")
+            if(result === true) {
+                console.log()
+                playGame()
+            } else {
+                return
+            }
+        })
+    })
+}
+
+
+
+playGame()
 
